961adfd463be305b71524c97535e0262630afeb7
griffinm <griffin.mahoney@gmail.com>
2017-10-20 16:23:29 -0400

    Fixes config; removes test stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var HandlebarsPlugin = require("handlebars-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -8,21 +9,24 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    publicPath: '/assets/',
+    filename: 'assets/[name].[hash:7].js'
   },
 
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style!css' },
+      { test: /\.handlebars$/, loader: 'handlebars-loader' }
     ]
   },
 
   plugins: [
-    new HandlebarsPlugin({
-      entry: path.join(process.cwd(), "src", "*.handlebars"),
-      output: path.join(process.cwd(), 'dist', '[name].html'),
-      partials: [path.join(process.cwd(), 'src', 'components', '*.handlebars')]
+    // new HandlebarsPlugin({
+    //   entry: path.join(process.cwd(), "src", "*.handlebars"),
+    //   output: path.join(process.cwd(), 'dist', '[name].html'),
+    //   partials: [path.join(process.cwd(), 'src', 'components', '*.handlebars')]
+    // }),
+    new HtmlWebpackPlugin({
+      template: '!!handlebars-loader!src/index.handlebars'
     })
   ]
 
